664dbacc870e7f40104b1341d2ff8fbc127fe431
Colin Bradshaw <colin.bradshaw@smoothstack.com>
2021-09-20 14:03:50 -0400

    better solution with streams better commenting

--- Day5/src/com/ss/dayfive/assignment2/NonNegativeRightNumber.java
+++ Day5/src/com/ss/dayfive/assignment2/NonNegativeRightNumber.java
@@ -6,6 +6,7 @@ package com.ss.dayfive.assignment2;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Colin Bradshaw
@@ -19,13 +20,16 @@ public class NonNegativeRightNumber {
 	public static void main(String[] args) {
 		List<Integer> l1 = Arrays.asList(11, 124, 1, 35895);
 		
-		l1.stream().forEach(n -> {
+		List<Integer> l2 = l1.stream().map((n) -> {
+			// remainder division until 1 digit long
 			while(n > 10) {
 				n %= 10;
 			}
-			System.out.println(n);
-		});
-
+			return n;
+			// collect to list
+		}).collect(Collectors.toList());
+		
+		System.out.println(l2.toString());
 	}
 
 }
--- Day5/src/com/ss/dayfive/assignment3/DoubleLambda.java
+++ Day5/src/com/ss/dayfive/assignment3/DoubleLambda.java
@@ -6,6 +6,7 @@ package com.ss.dayfive.assignment3;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Colin Bradshaw
@@ -17,19 +18,17 @@ public class DoubleLambda {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// given list of Integers
 		List<Integer> l1 = Arrays.asList(2, 7, 12, 24);
-		DoubleList dl = (i) -> {
-			 ArrayList<Integer> ret = new ArrayList<Integer>();
-			 for(Integer j: i) {
-				 int k = j*2;
-				 ret.add(k);
-			 }
-			 return ret;
-		};
-		
-		for(int i:dl.doubleContents(l1)) {
-			System.out.println(i);
-		}
+
+		List<Integer> l2 = l1.stream().map((n) -> {
+			// multiply by 2, return
+			n *=2;
+			return n;
+			//then collect as a list
+		}).collect(Collectors.toList());
+		// print
+		System.out.println(l2.toString());
 	}
 
 }
