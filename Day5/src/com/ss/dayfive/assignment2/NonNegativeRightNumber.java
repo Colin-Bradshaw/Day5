1e5ed4f7c8b0feea4b8c930d7f738808c3cca3c7
Colin Bradshaw <colin.bradshaw@smoothstack.com>
2021-09-20 13:43:58 -0400

    Better solution using a stream

--- Day5/src/com/ss/dayfive/assignment2/NonNegativeRightNumber.java
+++ Day5/src/com/ss/dayfive/assignment2/NonNegativeRightNumber.java
@@ -18,21 +18,13 @@ public class NonNegativeRightNumber {
 	 */
 	public static void main(String[] args) {
 		List<Integer> l1 = Arrays.asList(11, 124, 1, 35895);
-		RightmostDigit rd = (i) ->{
-			ArrayList<Integer> ret = new ArrayList<Integer>();
-			for(Integer j: i) {
-				int k = j.intValue();
-				while(k > 10) {
-					k %= 10;
-				}
-				ret.add(k);
-			}
-			return ret;
-		};
 		
-		for(int z: rd.rightmostDigit(l1)) {
-			System.out.println(z);
-		}
+		l1.stream().forEach(n -> {
+			while(n > 10) {
+				n %= 10;
+			}
+			System.out.println(n);
+		});
 
 	}
 
